b09598793daedce0d989059647bf680501972c73
Yertuntss <521yertunts@gmail.com>
2026-04-26 23:30:37 +0800

    2

--- src/components/titleScreen.jsx
+++ src/components/titleScreen.jsx
@@ -1,30 +1,25 @@
 import React from 'react'
+import { FiSearch } from "react-icons/fi";
 
-function titleScreen(){
+function TitleScreen(){
     return(
-        <div id="titleScreen">
-            <h1>Хурдан Шуурхай<b>Амархан</b></h1>
+        <div class="px-[80px] flex flex-col gap-[40px] justify-center items-center">
+            <p class="text-[64px] font-medium">Хурдан Шуурхай<b class="flex items-center justify-center font-bold text-[#005500]">Амархан</b></p>
             
-            <h2>Хамгийн ойрхон, хурднаар эмийн сан орж хэрэгтэй эмээ олж аваарай</h2>
-            <div id="titleSearchBar">
-                <div id="iconText">
-                    <button id="searchIcon">
-                            <i class="fa-solid fa-magnifying-glass"></i>
-                    </button>   
-                    <div id="titleSearchBox">
-                        <input type="text" id="titleSearchBarText" placeholder="Эм, эмийн сан хайх"></input>
-                    </div>
-                </div>
-                <h3 id="titleSearchBtn">Хайх</h3>
+            <p class="text-[20px] font-bold text-[#2090FF]">Хамгийн ойрхон, хурднаар эмийн сан орж хэрэгтэй эмээ олж аваарай</p>
+            <div class="flex items-center w-[858px] gap-[14px] h-[64px] rounded-[20px] border shadow-lg">
+                <FiSearch class="text-[20px] m-[14px]"/>   
+                <input type="text" class="w-full outline-none" placeholder="Эм, эмийн сан хайх"></input>
+                <h3 class="w-[96px] rounded-[20px] content-center text-center m-[8px] bg-[#005500] text-[16px] text-white h-[48px]">Хайх</h3>
             </div>  
-            <div id="titleMenu">
-                <p>Байршил</p>
-                <p>Эм найрлага</p>
-                <p>Нярай хүүхэд</p>
-                <p>Эм</p>
+            <div class="flex items-center gap-[24px]">
+                <p class="content-center font-semibold px-[24px] h-[32px] rounded-[20px] text-white bg-[#2090FF] text-[12px]">Байршил</p>
+                <p class="content-center font-semibold px-[24px] h-[32px] rounded-[20px] text-white bg-[#2090FF] text-[12px]">Эм найрлага</p>
+                <p class="content-center font-semibold px-[24px] h-[32px] rounded-[20px] text-white bg-[#2090FF] text-[12px]">Нярай хүүхэд</p>
+                <p class="content-center font-semibold px-[24px] h-[32px] rounded-[20px] text-white bg-[#2090FF] text-[12px]">Эм</p>
             </div>
         </div>
     )
 }
 
-export default titleScreen
+export default TitleScreen
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import Header from '../components/header'
+import TitleScreen from '../components/titleScreen'
+import EasyNavigation from '../components/easyNavigation'
+
 function Dashboard() {
   return (
     <div>
       <Header></Header>
+      <TitleScreen></TitleScreen>
+      <EasyNavigation></EasyNavigation>
     </div>
   )
 }
